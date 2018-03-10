64f2cf717ca589ab0c82fd7c3a2f01fe2c8f64f9
Vedran Knezevic <vedranknezevic1@yahoo.com>
2018-03-10 22:59:10 +0100

    commit

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "ComplexNumber.h"
+using namespace std;
+
+int main() {
+
+	ComplexNumber k1(17, 2);
+	ComplexNumber k2;
+	k2.set_real(9);
+	k2.set_imaginary(-2);
+	ComplexNumber rez(k1, k2);
+	cout << rez.get() << endl;
+	
+	return 0;
+}
