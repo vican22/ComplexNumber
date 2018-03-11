6dd4a2f9ccf59ab4f8b65e84797236d1302606ac
Vedran Knezevic <vedranknezevic1@yahoo.com>
2018-03-11 21:19:14 +0100

    commit

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -1,33 +1,34 @@
 #include "ComplexNumber.h"
 
-//konstruktor k1
-ComplexNumber::ComplexNumber(int r, int i) {
-	realni = r;
-	imaginarni = i;
+#include<sstream>
+using namespace std;
+
+ComplexNumber::ComplexNumber(int realni, int imaginarni) {
+	this->realni = realni;
+	this->imaginarni = imaginarni;
 }
 
-//konstruktor k2
+void ComplexNumber::set_real(int realni) {
+	this->realni = realni;
+}
+
+void ComplexNumber::set_imaginary(int imaginarni) {
+	this->imaginarni = imaginarni;
+}
+
 ComplexNumber::ComplexNumber() {
 	set_real(realni);
 	set_imaginary(imaginarni);
 }
 
-//konstruktor rez
-ComplexNumber::ComplexNumber(ComplexNumber k1, ComplexNumber k2) {
+ComplexNumber::ComplexNumber(ComplexNumber k1, ComplexNumber k2){
 	
-		realni = k1.realni + k2.realni;
-		imaginarni = k1.imaginarni + k2.imaginarni;
-}
-void ComplexNumber::set_real(int r) {
-	realni = r;
- }
-
-void ComplexNumber::set_imaginary(int i) {
-	imaginarni = i;
+	realni = k1.realni + k2.realni;
+	imaginarni = k1.imaginarni + k2.imaginarni;
 }
 
 string ComplexNumber::get() {
 	stringstream ss;
-	ss << "z = " << realni << " + " << imaginarni << "i";
+	ss << realni << " + " << imaginarni << "i";
 	return ss.str();
 }
