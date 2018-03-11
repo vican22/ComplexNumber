e3dcf924f64f782a5f6486eb570ef2cd79c98718
Vedran Knezevic <vedranknezevic1@yahoo.com>
2018-03-11 21:20:13 +0100

    commit

--- ComplexNumber.h
+++ ComplexNumber.h
@@ -1,31 +1,17 @@
 #pragma once
-
-#include<iostream>
 #include<string>
-#include<sstream>
 using namespace std;
 
-
-class ComplexNumber
-{
+class ComplexNumber {
 public:
-	//konstruktor k1
-	ComplexNumber(int r, int i);
-
-	//konstruktor za k2
+	ComplexNumber(int realni, int imaginarni);
+	void set_real(int realni);
+	void set_imaginary(int imaginarni);
 	ComplexNumber();
-
-	void set_real(int r);
-	void set_imaginary(int i);
-
-	//konstruktor za rez
-	ComplexNumber(ComplexNumber k1, ComplexNumber k2); 
-
+	ComplexNumber(ComplexNumber k, ComplexNumber k2);
 	string get();
 
-	
 private:
 	int realni;
 	int imaginarni;
 };
-
